f32b18e3eef71686995e16da26247d0e0f632df2
loozhengjie <loozhengjie555@gmail.com>
2024-01-22 20:21:45 +0800

    fixed preloader size issue

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ window.addEventListener('load', function() {
     setTimeout(() => {
         loader.classList.add("hidden");
         document.body.removeChild(loader);
-    }, 500);
+    }, 5000);
    
 })
 
